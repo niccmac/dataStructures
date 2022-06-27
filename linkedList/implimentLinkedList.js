8d43bafc7efe51f0bf7091cf14222af0931541f4
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-27 15:36:24 -0700

    Add list size

--- linkedList/implimentLinkedList.js
+++ linkedList/implimentLinkedList.js
@@ -1,3 +1,4 @@
+// Create class for nodes of list
 class ListNode {
   constructor(data) {
     this.data = data;
@@ -5,16 +6,32 @@ class ListNode {
   }
 }
 
+// Create class for list
 class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+  // Size of linked list
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
 }
 
+// Create nodes
 let node1 = new ListNode(2);
 let node2 = new ListNode(5);
+
+// Link nodes
 node1.next = node2;
 
+// Set node1 as head
 let list = new LinkedList(node1);
 
 console.log(list.head.next.data);
+console.log(list.size());
