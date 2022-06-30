5e73ece97c854a1516586ab81f6cafedcfe869d0
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-30 10:08:20 -0700

    add nodes, print nodes

--- binaryTree/binaryTreeImplement.js
+++ binaryTree/binaryTreeImplement.js
@@ -1,6 +1,6 @@
 class TreeNode {
   constructor(data) {
-    this.data;
+    this.data = data;
     this.left = null;
     this.right = null;
   }
@@ -34,4 +34,32 @@ class Tree {
       }
     }
   }
+
+  printInOrder() {
+    if (!this.root) {
+      console.log("Tree is empty!");
+    } else {
+      this.inOrderTraversal(this.root);
+    }
+  }
+
+  inOrderTraversal(node) {
+    // This traversal uses recursion
+    // console.log(node);
+    if (node.left) {
+      this.inOrderTraversal(node.left);
+    }
+    console.log(node.data);
+    if (node.right) {
+      this.inOrderTraversal(node.right);
+    }
+  }
 }
+
+const tree = new Tree();
+tree.printInOrder();
+tree.addNode(1);
+tree.addNode(0);
+tree.addNode(57);
+tree.addNode(4);
+tree.printInOrder();
